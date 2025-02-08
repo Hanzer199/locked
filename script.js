95ba05c737def9d6f30caac2c54736a4d0857caf
Hanzer199 <kingmuhafid@gmail.com>
2025-02-09 01:32:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ const countdown = () => {
         const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-        document.getElementById("days").innerText = days < 10 ? 0${days} : days;
-        document.getElementById("hours").innerText = hours < 10 ? 0${hours} : hours;
-        document.getElementById("minutes").innerText = minutes < 10 ? 0${minutes} : minutes;
-        document.getElementById("seconds").innerText = seconds < 10 ? 0${seconds} : seconds;
+        document.getElementById("days").innerText = days < 10 ? `0${days}` : days;
+        document.getElementById("hours").innerText = hours < 10 ? `0${hours}` : hours;
+        document.getElementById("minutes").innerText = minutes < 10 ? `0${minutes}` : minutes;
+        document.getElementById("seconds").innerText = seconds < 10 ? `0${seconds}` : seconds;
     } else {
         document.querySelector(".countdown").innerHTML = "<h2>Giveaway telah berakhir!</h2>";
     }
