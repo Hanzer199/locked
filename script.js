14a293d56c6d8b51e3b6d91af3ca4c6356f1134d
Hanzer199 <kingmuhafid@gmail.com>
2025-02-09 00:27:06 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("unlock-btn").addEventListener("click", function() {
-    this.innerHTML = "Mengalihkan...";
-    this.style.backgroundColor = "#ff6600";
-    setTimeout(() => {
-        window.location.href = "https://adbluemedia.com";
-    }, 2000);
-});
